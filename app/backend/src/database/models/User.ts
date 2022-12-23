f6652ddc8a322822466937e5d399e67cc6982758
Carlos Daniel <carlosdanielpereirasantos18@gmail.com>
2022-12-23 20:50:05 -0300

    adiciona sudo no comando compose:up:dev e cria model e migration de user

---
+++ app/backend/src/database/models/User.ts
@@ -0,0 +1,39 @@
+import { Model, INTEGER, STRING } from 'sequelize';
+import db from '.';
+// import OtherModel from './OtherModel';
+
+class User extends Model {
+  declare id: number;
+  declare username: string;
+  declare role: string;
+  declare email: string;
+  declare password: string;
+}
+
+User.init({
+  id: {
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+    type: INTEGER,
+  },
+  username: {
+    type: STRING,
+  },
+  role: {
+    type: STRING,
+  },
+  email: {
+    type: STRING,
+  },
+  password: {
+    type: STRING,
+  },
+}, {
+  underscored: true,
+  sequelize: db,
+  modelName: 'user',
+  timestamps: false,
+});
+
+export default User;
