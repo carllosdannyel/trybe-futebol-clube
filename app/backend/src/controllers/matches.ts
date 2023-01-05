a8897af915aa14feae9075440acb9b8059a33489
Carlos Daniel <carlosdanielpereirasantos18@gmail.com>
2023-01-05 02:10:29 -0300

    desenvolve a rota de matches onde nao pode adicionar partida sem time existente ou com token invalido

--- app/backend/src/controllers/matches.ts
+++ app/backend/src/controllers/matches.ts
@@ -6,7 +6,6 @@ export default class MatchesController {
 
   public async findMatches(req: Request, res: Response) {
     const { inProgress } = req.query;
-    console.log('inProgress', inProgress);
     const { status, message } = await this.matchesService
       .findMatches(inProgress as string);
     if (status) return res.status(status as number).json({ message });
--- app/backend/src/services/matches.ts
+++ app/backend/src/services/matches.ts
@@ -8,8 +8,6 @@ export default class MatchesService {
   private teams = Teams;
 
   public async findMatchesInProgress(query: string) {
-    console.log('findMatchesInProgress', query);
-
     const inProgress = query !== undefined && query === 'true';
 
     return this.matches.findAll({
@@ -37,26 +35,21 @@ export default class MatchesService {
     return { status: null, message: matchesInProgress };
   }
 
-  // private async findTeams(homeTeam: number, awayTeam: number) {
-  //   const teamHome = await this.teams.findByPk(homeTeam);
-  //   const teamAway = await this.teams.findByPk(awayTeam);
-  //   if (!teamHome?.dataValues || !teamAway?.dataValues) {
-  //     return { status: 404, message: 'There is no team with such id!' };
-  //   }
-  // }
-
-  // private static async areEqualTeams(homeTeam: number, awayTeam: number) {
-  //   if (homeTeam === awayTeam) {
-  //     return { status: 422, message: 'It is not possible to create a match with two equal teams' };
-  //   }
-  // }
+  private async findTeams(homeTeam: number, awayTeam: number) {
+    const teamHome = await this.teams.findByPk(homeTeam);
+    const teamAway = await this.teams.findByPk(awayTeam);
+    if (!teamHome || !teamAway) return true;
+    return false;
+  }
 
   public async createMatches(body: IMatcherCreate) {
     const { homeTeam, awayTeam, homeTeamGoals, awayTeamGoals } = body;
     if (homeTeam === awayTeam) {
       return { status: 422, message: 'It is not possible to create a match with two equal teams' };
     }
-    // await this.findTeams(homeTeam, awayTeam);
+    if (await this.findTeams(homeTeam, awayTeam)) {
+      return { status: 404, message: 'There is no team with such id!' };
+    }
     const match = await this.matches.create({
       homeTeam, awayTeam, homeTeamGoals, awayTeamGoals, inProgress: true,
     });
